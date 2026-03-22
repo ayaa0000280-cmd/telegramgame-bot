9f5036f869a0d15e04e6f55475395d28dbe5a1e5
ayaa0000280-cmd <ayaa000028.0@gmail.com>
2026-03-22 18:31:30 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+const TelegramBot = require('node-telegram-bot-api');
+const express = require('express');
+
+const app = express();
+const token = "حط_توكن_بوتك_هنا";
+const bot = new TelegramBot(token);
+
+bot.setWebHook("https://your-app.onrender.com/" + token);
+
+app.use(express.json());
+
+app.post("/" + token, (req, res) => {
+  bot.processUpdate(req.body);
+  res.sendStatus(200);
+});
+
+bot.on("inline_query", (q) => {
+  bot.answerInlineQuery(q.id, [{
+    type: "game",
+    id: "1",
+    game_short_name: "snake_game"
+  }]);
+});
+
+app.listen(3000, () => {
+  console.log("Bot is running");
+});
